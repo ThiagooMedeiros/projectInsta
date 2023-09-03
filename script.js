07f3a826a42b968d9e2425b52ff52dc5272c4c0c
Thiago <thiagoodemedeiros@gmail.com>
2023-09-03 12:30:37 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle(`light`)
+  const img = document.querySelector(`#profile img`)
+  if (html.classList.contains(`light`)) {
+    img.setAttribute(`src`, `./assets/assets/th2.png`)
+  } else {
+    img.setAttribute(`src`, `./assets/assets/th.png`)
+  }
+}
